1a0dba2f7f59a4debe236cd5a439360127881f7e
Tianchai Riengviwat <tianchai.riengviwat@gmail.com>
2021-05-30 14:40:41 +0700

    System 2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,7 +19,7 @@
       <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
       <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News 1</a></li>
     </ul>
-    <h3>Global Ecosystem</h3>
+    <h3>Global Ecosystem 2</h3>
     <ul>
       <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
       <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
